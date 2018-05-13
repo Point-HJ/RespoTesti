a5d31d58fd286a96a70618664e64e5701e124c36
Heidi Joenpolvi <heidi.joenpolvi@stud.pointcollege.fi>
2018-05-13 13:20:26 +0300

    Testicontoller luotu, Index-sivua muokattu Bootstrapin avulla, mm navigointipalkki

---
+++ RespoTesti/RespoTesti/Controllers/TestiController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RespoTesti.Controllers
+{
+    public class TestiController : Controller
+    {
+        // GET: Testi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Testi/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Testi/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Testi/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Testi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Testi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Testi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Testi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
